1aaa75d526679a52b10cc533e954158dd6cbd816
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2024-09-20 16:43:47 +0200

    Changed: simplified logging a bit.

--- src/frontend-rust/src/main.rs
+++ src/frontend-rust/src/main.rs
@@ -29,83 +29,87 @@ fn generate_request_id() -> String {
 
 // Helper function to read a file's contents
 fn read_file(file_path: &str) -> Option<String> {
-    debug!(function = "read_file", file_path = file_path, "Enter");
+    let function = "read_file";
+    debug!(function, file_path, "Enter");
 
     let mut file = match File::open(file_path) {
         Ok(file) => file,
         Err(_) => {
-            warn!(file_path = file_path, "File not found");
+            warn!(function, file_path, "File not found");
             return None;
         }
     };
 
     let mut contents = String::new();
     if file.read_to_string(&mut contents).is_ok() {
-        debug!(file_path = file_path, "Read file successful");
+        debug!(function, file_path, "Read file successful");
         Some(contents)
     } else {
-        warn!(file_path = file_path, "Failed to read file");
+        warn!(function, file_path, "Failed to read file");
         None
     }
 }
 
 fn send_fibonacci_request(number: i32, request_id: &str) -> Result<FibonacciResponse, Box<dyn std::error::Error>> {
-    let url = "http://192.168.6.32/fibonacci";
-    debug!(function = "send_fibonacci_request", request_id = request_id, backend_url = url,
-        number = number, "Sending request");
+    let function = "send_fibonacci_request";
+    let backend_url = "http://192.168.6.32/fibonacci";
+    debug!(function, request_id, backend_url, number, "Sending request");
 
     let payload = json!({
         "number": number
     });
 
-    let response = attohttpc::get(url)
+    let response = attohttpc::get(backend_url)
         .header("X-Request-ID", request_id)
         .json(&payload)?
         .send()?;
 
     let response_text = response.text()?;
-    debug!(function = "send_fibonacci_request", request_id = request_id,
-        backend_response = response_text.as_str(), "Response received");
+    debug!(function, request_id, backend_response = response_text.as_str(), "Response received");
 
     let fib_response: FibonacciResponse = serde_json::from_str(&response_text)?;
-    debug!(function = "send_fibonacci_request", request_id = request_id,
-        answer = fib_response.result, "Answer received");
+    debug!(function, request_id, answer = fib_response.result, "Answer received");
 
     Ok(fib_response)
 }
 
 fn handle_fibonacci_request(request: &Request) -> Response {
+    let function = "handle_fibonacci_request";
     let request_id = generate_request_id();
+    debug!(function, request_id, "Enter");
 
     // Read the request body (JSON payload)
     let mut body = String::new();
     if let Some(mut data) = request.data() {
-        if let Err(_) = data.read_to_string(&mut body) {
+        if let Err(err) = data.read_to_string(&mut body) {
+            error!(error = %err, function, request_id, "Failed to read request body");
             return Response::text("Failed to read request body").with_status_code(400);
         }
     } else {
+        error!(function, request_id, "No request body found");
         return Response::text("No request body found").with_status_code(400);
     }
 
     // Parse the JSON body
     let fib_request: FibonacciRequest = match serde_json::from_str(&body) {
         Ok(fib_request) => fib_request,
-        Err(_) => return Response::text("Invalid JSON").with_status_code(400),
+        Err(err) => {
+            error!(error = %err, function, request_id, "Invalid JSON");
+            return Response::text("Invalid JSON").with_status_code(400)
+        },
     };
 
     // Send the request to the backend
     match send_fibonacci_request(fib_request.number, request_id.as_str()) {
         Ok(fib_response) => {
-            debug!(function = "handle_fibonacci_request", request_id = request_id,
-                "Response received");
+            debug!(function, request_id, "Response received");
             // Convert the response to JSON and return it
             let response_json = serde_json::to_string(&fib_response).unwrap();
             Response::text(response_json)
                 .with_additional_header("Content-Type", "application/json")
         }
         Err(error) => {
-            error!(function = "handle_fibonacci_request", request_id = request_id, error = error,
-                "Failed to send fibonacci request");
+            error!(error = %error, function, request_id, "Failed to send fibonacci request");
             Response::text("Failed to contact backend").with_status_code(500)
         },
     }
@@ -115,7 +119,10 @@ fn main() {
                                                  "/var/log/fibonacci",
                                                  "frontend");
     tracing_subscriber::fmt()
-        .json()  // Output logs as JSON
+        .json()
+        .flatten_event(true)// Put fields at the root level
+        .with_current_span(false)
+        .with_span_list(false)
         .with_writer(file_appender)
         .with_env_filter(EnvFilter::from_default_env())
         .with_max_level(Level::DEBUG)
