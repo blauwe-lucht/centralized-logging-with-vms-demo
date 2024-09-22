4f1b4197e7af7f5dee3ccb678389a0f75b6be66c
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2024-09-22 10:19:21 +0200

    Added: logging of backend to opensearch.

--- configure-backend.sh
+++ configure-backend.sh
@@ -85,41 +85,12 @@ server {
     }
 }
 EOF
-cat > /etc/nginx/nginx.conf <<EOF
-user nginx;
-worker_processes auto;
-error_log /var/log/nginx/error.log;
-pid /run/nginx.pid;
-
-# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
-include /usr/share/nginx/modules/*.conf;
-
-events {
-    worker_connections 1024;
-}
-
-http {
-    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '\$status \$body_bytes_sent "\$http_referer" '
-                      '"\$http_user_agent" "\$http_x_forwarded_for"';
-
-    access_log  /var/log/nginx/access.log  main;
-
-    sendfile            on;
-    tcp_nopush          on;
-    tcp_nodelay         on;
-    keepalive_timeout   65;
-    types_hash_max_size 2048;
-
-    include             /etc/nginx/mime.types;
-    default_type        application/octet-stream;
-
-    # Load modular configuration files from the /etc/nginx/conf.d directory.
-    # See http://nginx.org/en/docs/ngx_core_module.html#include
-    # for more information.
-    include /etc/nginx/conf.d/*.conf;
-}
-EOF
+if [ -f /tmp/nginx.conf ]; then
+    rm -f /tmp/nginx.conf
+fi
+cp /vagrant/nginx/nginx.conf /tmp
+chown root: /tmp/nginx.conf /tmp/nginx.conf
+mv /tmp/nginx.conf /etc/nginx/nginx.conf
 
 echo Starting nginx...
 systemctl enable nginx
@@ -128,3 +99,21 @@ systemctl restart nginx
 echo Opening up port 80...
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
+
+echo Installing fluentbit...
+curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
+
+echo Configuring fluentbit...
+mkdir -p /var/lib/fluent-bit
+if [ -f /tmp/fluent-bit.conf ]; then
+    rm -f /tmp/fluent-bit.conf
+fi
+cp /vagrant/fluent-bit/backend/fluent-bit.conf /tmp
+cp /vagrant/fluent-bit/parsers.conf /tmp
+chown root: /tmp/fluent-bit.conf /tmp/parsers.conf
+mv /tmp/fluent-bit.conf /etc/fluent-bit/fluent-bit.conf
+mv /tmp/parsers.conf /etc/fluent-bit/parsers.conf
+
+echo Starting fluentbit...
+systemctl enable fluent-bit
+systemctl restart fluent-bit
