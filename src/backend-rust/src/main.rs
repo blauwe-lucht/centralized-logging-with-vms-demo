e47d50ad0465d46c420601cd85f09c433511e7bb
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2024-09-22 07:26:00 +0200

    Changed: refactor backend.

--- src/backend-rust/src/main.rs
+++ src/backend-rust/src/main.rs
@@ -39,18 +39,19 @@ fn calculate_fibonacci(n: i32) -> i64 {
 
 fn extract_request_id(request: &Request) -> String {
     const REQUEST_ID_HEADER: &str = "X-Request-ID";
-    if let Some(header) = request.header(REQUEST_ID_HEADER) {
-        debug!(request_id = %header, "Request ID found in headers");
-        header.to_string()
+    if let Some(request_id) = request.header(REQUEST_ID_HEADER) {
+        debug!(request_id = %request_id, "Request ID found in headers");
+        request_id.to_string()
     } else {
-        let new_request_id = "unknown";
-        warn!(request_id = %new_request_id, "No Request ID found in headers, using 'unknown'");
-        new_request_id.to_string()
+        let request_id = "unknown";
+        warn!(request_id = %request_id, "No Request ID found in headers, using 'unknown'");
+        request_id.to_string()
     }
 }
 
 fn handle_fibonacci_request(request: &Request) -> Response {
-    info!(function = "handle_fibonacci_request", "Start");
+    let function = "handle_fibonacci_request";
+    info!(function, "Start");
 
     let request_id = extract_request_id(request);
 
@@ -64,7 +65,7 @@ fn handle_fibonacci_request(request: &Request) -> Response {
         Err(value) => return value,
     };
 
-    debug!(request_id = request_id, number = fib_request.number, "Calculating Fibonacci");
+    debug!(function, request_id, number = fib_request.number, "Calculating Fibonacci");
 
     let result = calculate_fibonacci(fib_request.number);
     let fib_response = FibonacciResponse {
@@ -73,16 +74,17 @@ fn handle_fibonacci_request(request: &Request) -> Response {
         request_id: request_id.clone(),
     };
 
-    info!(request_id = %request_id, result = result, "Fibonacci result calculated");
+    info!(function, request_id, result, "Fibonacci result calculated");
 
     Response::json(&fib_response)
 }
 
 fn parse_body_text(body: &String) -> Result<FibonacciRequest, Response> {
+    let function = "parse_body_text";
     let fib_request: FibonacciRequest = match serde_json::from_str(&body) {
         Ok(req) => req,
         Err(err) => {
-            warn!(function = "handle_fibonacci_request", body = body, error = %err, "Invalid JSON in request");
+            warn!(function, body, error = %err, "Invalid JSON in request");
             return Err(Response::text("Invalid JSON").with_status_code(400));
         }
     };
@@ -90,14 +92,15 @@ fn parse_body_text(body: &String) -> Result<FibonacciRequest, Response> {
 }
 
 fn extract_request_body_text(request: &Request) -> Result<String, Response> {
+    let function = "extract_request_body_text";
     let mut body = String::new();
     if let Some(mut data) = request.data() {
         if let Err(err) = data.read_to_string(&mut body) {
-            warn!(function = "handle_fibonacci_request", error = %err, "Failed to read request body");
+            warn!(function, error = %err, "Failed to read request body");
             return Err(Response::text("Failed to read request body").with_status_code(400));
         }
     } else {
-        warn!(function = "handle_fibonacci_request", "No request body found");
+        warn!(function, "No request body found");
         return Err(Response::text("No request body found").with_status_code(400));
     }
     Ok(body)
