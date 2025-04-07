577affacbd274127a5a1f8efaf2e685263ed42f6
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2025-04-07 09:49:21 +0200

    Changed: rebuild.sh can only be run as root. Since cargo has only been installed for root.

--- src/backend-rust/rebuild.sh
+++ src/backend-rust/rebuild.sh
@@ -2,9 +2,14 @@
 
 set -euo pipefail
 
-sudo systemctl stop fibonacci-backend
-cd /vagrant/src/backend-rust
-cargo build
-sudo cp /vagrant/src/backend-rust/target/debug/fibonacci_backend /usr/local/bin/fibonacci/
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (cargo has only been installed for root)"
+  exit 1
+fi
 
-sudo systemctl start fibonacci-backend
+systemctl stop fibonacci-backend
+cd /vagrant/src/backend-rust
+/root/.cargo/bin/cargo build
+cp /vagrant/src/backend-rust/target/debug/fibonacci_backend /usr/local/bin/fibonacci/
+
+systemctl start fibonacci-backend
